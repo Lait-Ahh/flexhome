bc9dcca1608a45623f38e9d741bdbe09fb22f9da
Lait-Ahh <73257805+Lait-Ahh@users.noreply.github.com>
2022-12-19 22:57:19 +0100

    Add Menu

--- ts/main.ts
+++ ts/main.ts
@@ -1,14 +1,98 @@
-import { app, ipcMain, BrowserWindow } from 'electron';
+import { app, ipcMain, BrowserWindow, Menu } from 'electron';
 import * as path from 'path';
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
-        autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true,
             preload: path.join(__dirname + '/preload.js')
         }
     });
+
+    const menu = Menu.buildFromTemplate([
+        {
+            label: 'Fichier',
+            submenu: [
+                {
+                    label: 'Nouveau projet',
+                    sublabel: 'Ctrl+Alt+Windows+N'
+                },
+                {
+                    label: 'Ouvrir un projet',
+                    sublabel: 'Ctrl+O'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Enregistrer',
+                    sublabel: 'Ctrl+S'
+                },
+                {
+                    label: 'Enregistrer sous...',
+                    sublabel: 'Ctrl+Maj+S'
+                },
+                {
+                    label: 'Enregistrement automatique',
+                    type: 'checkbox'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Fermer la fenêtre',
+                    sublabel: 'Alt+F4'
+                }
+            ]
+        },
+        {
+            label: 'Edition',
+            submenu: [
+                {
+                    label: 'Annuler',
+                    sublabel: 'Ctrl+Z'
+                },
+                {
+                    label: 'Rétablir',
+                    sublabel: 'Ctrl+Y'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Couper',
+                    sublabel: 'Ctrl+X'
+                },
+                {
+                    label: 'Copier',
+                    sublabel: 'Ctrl+C'
+                },
+                {
+                    label: 'Coller',
+                    sublabel: 'Ctrl+V'
+                }
+            ]
+        },
+        {
+            label: 'Préférences',
+            submenu: [
+                {
+                    label: 'Paramètres MIDI'
+                },
+                {
+                    label: 'Paramètres audios'
+                },
+                {
+                    label: 'Paramètres généraux'
+                },
+                {
+                    label: 'Paramètres du projet'
+                }
+            ]
+        }
+    ]);
+
+    mainWindow.setMenu(menu);
     mainWindow.loadFile(path.join(__dirname + '/../index.html'));
 }
 
